dd8b7fe462f6284a3000101df6cf434b25d51a2f
Victor Pavas <victorpavas@gmail.com>
2026-02-22 16:55:26 -0500

    Corregir nombre del archivo

---
+++ elevar_al_cubo.py
@@ -0,0 +1,10 @@
+# Programa para elevar un número al cubo
+
+# Pedir el número al usuario
+numero = float(input("Ingrese un número: "))
+
+# Calcular el cubo
+cubo = numero ** 3
+
+# Mostrar el resultado
+print("El cubo de", numero, "es:", cubo)
